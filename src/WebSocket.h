7298e4aa0fd17c161c40c41279a8dabe66bf5639
Kyle Shank <kyle.shank@gmail.com>
2020-12-13 14:04:49 -0500

    Lets also cork

--- src/WebSocket.h
+++ src/WebSocket.h
@@ -57,6 +57,7 @@ public:
     using Super::close;
 
     /* Lets un-cork */
+    using Super::cork;
     using Super::uncork;
 
     /* Send or buffer a WebSocket frame, compressed or not. Returns false on increased user space backpressure. */
